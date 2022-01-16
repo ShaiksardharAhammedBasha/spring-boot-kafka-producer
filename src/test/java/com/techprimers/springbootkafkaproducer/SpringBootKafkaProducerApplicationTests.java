782a99a76fbde4eda75437f1a08a4b513406b9f9
ShaiksardharAhammedBasha <smabasha222@gmail.com>
2022-01-16 16:26:08 +0530

    modified pom.xml with Junit dependencies and added new test case

--- src/test/java/com/techprimers/springbootkafkaproducer/SpringBootKafkaProducerApplicationTests.java
+++ src/test/java/com/techprimers/springbootkafkaproducer/SpringBootKafkaProducerApplicationTests.java
@@ -1,13 +1,17 @@
 package com.techprimers.springbootkafkaproducer;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootKafkaProducerApplicationTests {
 
+	int a = 20; int b = 20;
+
 	@Test
-	void contextLoads() {
+	public void method() {
+		Assert.assertTrue("OMG test!", true);
 	}
 
 }
