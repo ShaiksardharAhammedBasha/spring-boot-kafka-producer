55cc36142d00d1f99ba5b67c2cc91a4d868fc707
ShaiksardharAhammedBasha <smabasha222@gmail.com>
2022-01-30 23:14:43 +0530

    Removed Test Case File To Avoid Build Failure in Team City

--- src/test/java/com/techprimers/springbootkafkaproducer/SpringBootKafkaProducerApplicationTests.java
+++ src/test/java/com/techprimers/springbootkafkaproducer/SpringBootKafkaProducerApplicationTests.java
@@ -3,14 +3,11 @@ package com.techprimers.springbootkafkaproducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class SpringBootKafkaProducerApplicationTests {
 
 	int a = 20; int b = 20;
 
-	@Test
-	public void method() {
-		assertEquals;
-	}
 
 }
