59de41942473d79795a3cab58969c2f53f2dfdb0
ShaiksardharAhammedBasha <smabasha222@gmail.com>
2022-02-06 19:19:32 +0530

    Added Test Cases in Demo Branch

--- src/test/java/com/techprimers/springbootkafkaproducer/SpringBootKafkaProducerApplicationTests.java
+++ src/test/java/com/techprimers/springbootkafkaproducer/SpringBootKafkaProducerApplicationTests.java
@@ -3,11 +3,16 @@ package com.techprimers.springbootkafkaproducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class SpringBootKafkaProducerApplicationTests {
 
-	int a = 20; int b = 20;
+	int a = 20; int b = 21;
 
-
+	@Test
+	public void testAAndBEqualsCheckInDemoBranch() {
+		assertEquals(a, b);
+	}
 }
